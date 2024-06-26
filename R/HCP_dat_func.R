8be54a180bb440616552b8c7e3f144c2c937193f
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2024-06-26 22:21:17 +0800

    Update HCP_dat_func.R

--- R/HCP_dat_func.R
+++ R/HCP_dat_func.R
@@ -92,7 +92,7 @@ extractFC=function(wb_path,
 {
   ##check base_dir and sub.list
   
-  cat("ExtractFC tool: last updated 26/6/2024 10.10pm\n checking directory structure...\n")
+  cat("ExtractFC tool: last updated 26/6/2024 10.20pm\n checking directory structure...\n")
   if(!dir.exists(base_dir))  {stop(paste("The base directory '",base_dir,"' does not exist. Please check if you are at the correct working directory",sep=""))}
   sub.list=list.dirs(base_dir,recursive=F,full.names=F)
   N.orig=length(sub.list)
@@ -402,7 +402,7 @@ extractFC=function(wb_path,
 
   for (sub in 1:length(headmotion.output.filelist))
   {
-    report.all[sub,]=read.table(headmotion.output.filelist[sub],header = F,sep=",")[1,]
+    report.all[sub,]=read.table(paste0(output_dir,"/headmotion/",headmotion.output.filelist[sub]),header = F,sep=",")[1,]
   }
   write.table(report.all,file=paste0(output_dir,"/report.csv"),row.names = F,sep=",")
 }
