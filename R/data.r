784e55f97832d69b2898c0e345a684fd2c4c5618
Charly Hugo Alexandre Billaud <charlybillaud@hotmail.fr>
2024-10-10 11:11:20 +0800

    Rdoc update
    Also documenting labels_dat.Rd object so it can be exported via FCtools::...

---
+++ R/data.r
@@ -0,0 +1,14 @@
+#' Labels data
+#'
+#' A list containing four data frames, listing atlas labels, index numbers, hemispheres and lobes they correspond to, for each available atlas (1=AAL-90 atlas, 2=Schaefer-100 atlas + 19 subcortical regions, 3=Schaefer-200 atlas + 19 subcortical regions, 4=Brainnetome atlas). 
+#'
+#' @format ## `labels_dat`
+#' A list of four data.frames: () 
+#' \describe{
+#'   \item{vertices}{data frame with 90 rows (atlas parcellations), 6 columns (atlas label, region number, index order (old), index order (new), hemisphere (1=L,2=R), region label (anatomical area name) )}
+#'   \item{vertices}{data frame with 119 rows (atlas parcellations), 6 columns (atlas label, region number, index order (old), index order (new), hemisphere (1=L,2=R), region label (anatomical area name) )}
+#'   \item{vertices}{data frame with 219 rows (atlas parcellations), 6 columns (atlas label, region number, index order (old), index order (new), hemisphere (1=L,2=R), region label (anatomical area name) )}
+#'   \item{vertices}{data frame with 246 rows (atlas parcellations), 6 columns (atlas label, region number, index order (old), index order (new), hemisphere (1=L,2=R), region label (anatomical area name) )}
+#' }
+"labels_dat"
+
