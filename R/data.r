c5d5a41d10b854bdb9d544567c61d24b98d32e8a
Charly Hugo Alexandre Billaud <charlybillaud@hotmail.fr>
2024-11-18 17:36:49 +0800

    get() was not sufficient to use the function without loading FCtools into the library. Additional tweaks to fix this

--- R/data.r
+++ R/data.r
@@ -2,6 +2,9 @@
 #'
 #' A list containing four data frames, listing atlas labels, index numbers, hemispheres and lobes they correspond to, for each available atlas (1=AAL-90 atlas, 2=Schaefer-100 atlas + 19 subcortical regions, 3=Schaefer-200 atlas + 19 subcortical regions, 4=Brainnetome atlas). 
 #'
+#' @name labels_dat
+#' @docType data 
+#' @keywords internal 
 #' @format ## `labels_dat`
 #' A list of four data.frames: () 
 #' \describe{
@@ -10,5 +13,4 @@
 #'   \item{vertices}{data frame with 219 rows (atlas parcellations), 6 columns (atlas label, region number, index order (old), index order (new), hemisphere (1=L,2=R), region label (anatomical area name) )}
 #'   \item{vertices}{data frame with 246 rows (atlas parcellations), 6 columns (atlas label, region number, index order (old), index order (new), hemisphere (1=L,2=R), region label (anatomical area name) )}
 #' }
-"labels_dat"
-
+'labels_dat'
