7d0d2ea546804d680033e5b82913fde97e497dc8
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2025-06-19 12:25:37 +0800

    Create headmotion_XCP.R

---
+++ R/headmotion_XCP.R
@@ -0,0 +1,52 @@
+#' @title headmotion.XCP
+#'
+#' @description Calculating mean FD from XCP outputs and generating bash script to remove fMRI runs with excessive headmotion
+#'
+#' @details This function calculating mean FD from XCP outputs and generating bash script to remove fMRI runs with excessive headmotion. This is used mainly on the XCP-d post-processed HCP fMRI data.
+#'
+#' @param threshold mean FD threshold for identifying fMRI runs for deletion due to excessive headmotion. Set to `0.25` by default
+#' @param filename Filename of the output.csv file. Set to `FD.csv` by default
+#' @returns outputs a .csv file containing the columns of fMRI runs and FD, and a bash script `del.sh`
+#'
+#' @examples
+#' \dontrun{
+#' headmotion.XCP()
+#' }
+#' @export
+
+########################################################################################################
+########################################################################################################
+
+headmotion.XCP=function(filename="FD.csv", threshold=0.25)
+{
+  filelist=list.files(pattern="_motion.tsv", recursive=T)
+  FD.all=data.frame(matrix(NA,nrow=length(filelist),ncol=2))
+  colnames(FD.all)=c("fMRI_run","FD")
+  
+  for (file in 1:length(filelist))
+  {
+    FD.all$fMRI_run[file]=basename(filelist[file])
+    FD.all$FD[file]=mean(read.table(filelist[file],header=T)$framewise_displacement)
+  }
+  
+  files.del=FD.all[which(FD.all$FD>threshold),1]
+  del.sh=paste0("rm -rf ",gsub("_motion.tsv","*",files.del))
+  
+  freq=table(dirname(dirname(filelist)))
+  freq=paste0(names(freq),"-",as.numeric(freq))
+  
+  freq.del=table(dirname(dirname(files.del)))
+  freq.del.rm=paste0(names(freq.del),"-",as.numeric(freq.del))
+  
+  idx.match=match(c(freq.del),freq)
+  idx.del=which(!is.na(idx.match))
+  if(length(idx.del)>1)
+  {
+    del.sh=c(del.sh,paste0("rm -rf ",names(freq.del)[idx.del],"*"))
+    cat(del.sh,sep = "\n") 
+    write.table(del.sh,row.names=F, col.names=F,quote=F, file="del.sh")
+  }
+  FD.all$fMRI_run=gsub(pattern="_motion.tsv",replacement="",x=FD.all$fMRI_run)
+  write.table(FD.all, row.names=F, quote=F, sep=",", file="filename")
+}
+
