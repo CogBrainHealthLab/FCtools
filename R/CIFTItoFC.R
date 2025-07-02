32cfa24a0a623136d74a84f6c116a6eafbfe41c0
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2025-07-02 14:45:32 +0800

    Update CIFTItoFC.R

--- R/CIFTItoFC.R
+++ R/CIFTItoFC.R
@@ -143,7 +143,7 @@ CIFTItoFC=function(path="./",wb_path="/home/junhong.yu/workbench/bin_rh_linux64"
   {
     if(concat_subj==T)
     {
-      saveRDS(list(dirname(sublist),all_TS),file=filename)  
+      saveRDS(list(sublist,all_TS),file=filename)  
     } else if (concat_subj==F)
     {
       saveRDS(list(basename(sublist),all_TS),file=filename)  
