0e5756661dae9c136ac0f372f2041620d7aa2578
CogBrainHealthLab <148365456+CogBrainHealthLab@users.noreply.github.com>
2025-10-12 14:02:01 +0800

    Update HCP_dat_func.R

--- R/HCP_dat_func.R
+++ R/HCP_dat_func.R
@@ -25,52 +25,9 @@
 
 extract_linksXCP=function(manifest="datastructure_manifest.txt",task,surf,filename="downloadlist.txt", subjects)
 {
-  files=list()
-  if(!missing(task))
-  {
-    files=c("Atlas_MSMAll.dtseries.nii",
-            "brainmask_fs.2.nii.gz",
-            "_SBRef.nii.gz",
-            "Movement_Regressors.txt",
-            "Movement_AbsoluteRMS.txt",
-            "MNINonLinear/T1w.nii.gz",
-            "MNINonLinear/ribbon.nii.gz",
-            "MNINonLinear/aparc\\+aseg",
-            "MNINonLinear/T1w.nii.gz",
-            "MNINonLinear/brainmask_fs.nii.gz",
-            paste0("_",task,"?_...nii.gz"))
-  }
-  if(!missing(surf) & !missing(task))
-  {
-    files=c(files,"lh.cortex.label",
-            "rh.cortex.label",
-            "lh.sphere.reg",
-            "rh.sphere.reg",
-            paste0("lh.",surf),
-            paste0("rh.",surf),
-            "aseg.stats")
-  } else if(!missing(surf) & missing(task))
-  {
-    files=c("lh.cortex.label",
-            "rh.cortex.label",
-            "lh.sphere.reg",
-            "rh.sphere.reg",
-            paste0("lh.",surf),
-            paste0("rh.",surf),
-            "aseg.stats")
-  }
   #read manifest and remove first row; the first row contains description of the column, hence not used.
   manifest=read.table(manifest,header = T)[-1,]
-  
-  #identify indices of the associated file
-  idx.list=list()
-  for(file in 1:NROW(files))
-  {
-    idx.list[[file]]=which(stringr::str_detect(pattern = files[file],string = manifest$associated_file)==T)
-  }
-  
-  filelist=manifest$associated_file[unique(unlist(idx.list))]
-  
+
   #select subjects
   if(!missing(subjects))
   {
@@ -78,40 +35,91 @@ extract_linksXCP=function(manifest="datastructure_manifest.txt",task,surf,filena
     {
       if(sub==1)
       {
-        filelist.sel=filelist[stringr::str_detect(string=filelist,pattern =subjects[sub])]
+        filelist.incsub=manifest$associated_file[stringr::str_detect(string=manifest$associated_file,pattern =subjects[sub])]
       } else
       {
-        filelist.sel=c(filelist.sel,filelist[stringr::str_detect(string=filelist,pattern =subjects[sub])])  
+        filelist.incsub=c(filelist.incsub,manifest[stringr::str_detect(string=manifest$associated_file,pattern =subjects[sub])])  
       }
     }
   } else
   {
-    filelist.sel.alltasks=filelist
+    filelist.incsub=manifest$associated_file
   }
   
-  #remove MNINonLinear/brainmask_fs.2.nii.gz
   if(!missing(task))
   {
-    filelist.sel.alltasks=filelist.sel.alltasks[-stringr::str_detect(string=filelist.sel.alltasks,pattern ="MNINonLinear/brainmask_fs.2.nii.gz")]    
-    #filter tasks
-    for (t in 1:length(task))
+    #select required task-related files
+    files=c("Atlas_MSMAll.dtseries.nii",
+            "brainmask_fs.2.nii.gz",
+            "_SBRef.nii.gz",
+            "Movement_Regressors.txt",
+            "Movement_AbsoluteRMS.txt",
+            paste0("_",task,"?_...nii.gz"))
+    
+    idx.list=list()
+    for(file in 1:NROW(files))
     {
-    if(t==1)
-      {
-        filelist.sel=filelist.sel.alltasks[stringr::str_detect(string=filelist.sel.alltasks,pattern =task[1])]      
-      } else
-      {
-        filelist.sel=c(filelist.sel,filelist.sel.alltasks[stringr::str_detect(string=filelist.sel.alltasks,pattern =task[t])])      
-      }
+      idx.list[[file]]=which(stringr::str_detect(pattern = files[file],string = filelist.incsub)==T)
     }
+    
+    filelist.alltasks=filelist.incsub[unique(unlist(idx.list))]
+    filelist.alltasks=filelist.alltasks[-stringr::str_detect(string=filelist.alltasks,pattern ="MNINonLinear/brainmask_fs.2.nii.gz")]    
+    
+    #filter tasks
+    task.idx.list=list()
+    for(t in 1:length(task))
+    {
+      task.idx.list[[file]]=which(stringr::str_detect(pattern = task[t],string = filelist.alltasks)==T)
+    }
+    filelist.sel.tasks=filelist.alltasks[unique(unlist(task.idx.list))]
+    
+    #add MNINonlinear files
+    MNIfiles=c("MNINonLinear/T1w.nii.gz",
+               "MNINonLinear/ribbon.nii.gz",
+               "MNINonLinear/aparc\\+aseg",
+               "MNINonLinear/T1w.nii.gz",
+               "MNINonLinear/brainmask_fs.nii.gz")
+    MNI.idx.list=list()
+    for(file in 1:length(MNIfiles))
+    {
+      MNI.idx.list[[file]]=which(stringr::str_detect(pattern = MNIfiles[file],string = filelist.incsub)==T)
+    }
+    filelist.MNI=filelist.incsub[unique(unlist(MNI.idx.list))]
+    filelist.sel.tasks.MNI=c(filelist.sel.tasks,filelist.MNI)
+  }
+  #add surface files
+  if(!missing(surf))
+  {
+    surf_files=c("lh.cortex.label",
+            "rh.cortex.label",
+            "lh.sphere.reg",
+            "rh.sphere.reg",
+            paste0("lh.",surf),
+            paste0("rh.",surf),
+            "aseg.stats")
+    surf.idx.list=list()
+    for(file in 1:NROW(files))
+    {
+      surf.idx.list[[file]]=which(stringr::str_detect(pattern = surf_files[file],string = filelist.incsub)==T)
+    }
+    filelist.surf=filelist.incsub[unique(unlist(surf.idx.list))]
+
+    if(!missing(task))
+      {
+      filelist.sel.tasks.MNI=c(filelist.surf,filelist.sel.tasks.MNI)
+      }
+    else
+      {
+      filelist.sel.tasks.MNI=filelist.surf
+      }
   }
   #check if files were found
-  if(length(filelist.sel)==0)
+  if(length(filelist.sel.tasks)==0)
   {
     stop("No files were found, check your task name")
   }
   #output filelist as a text file
-  write.table(filelist.sel,file=filename, quote = F, row.names = F, col.names = F)
+  write.table(filelist.sel.tasks[order(filelist.sel.tasks)],file=filename, quote = F, row.names = F, col.names = F)
 }
 
 
